e7b3b8002ab0947c4c8efe243f7182983b62b39b
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2025-01-04 18:17:52 +0500

    first commit

---
+++ src/Components/Main/Notes/Notes.jsx
@@ -0,0 +1,12 @@
+// Style
+import "./Notes.css";
+
+function Notes() {
+  return (
+    <div className="notes">
+      <div className="notes_container container"></div>
+    </div>
+  );
+}
+
+export default Notes;
